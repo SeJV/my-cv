8dc06bee60b6c375887517e3c2cf270fd43f4b30
Sebastian Vogt <sebastian1210@web.de>
2021-03-06 15:44:48 +0100

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 import HomeCard from '../components/HomeCard'
 
@@ -12,9 +13,11 @@ function Home() {
             <div className="aboutme">
                 <h1>Hello</h1>
                 <h2>Who I am and what I do</h2>
-                <Link to="/cv">Curriculum Vitae</Link>
-                <Link to="/projects">Projects</Link>
-                <p>Description Text about me</p>
+                <span className="links">
+                    <Link to="/cv"><Button>CURRICULUM VITAE</Button></Link>
+                    <Link to="/projects"><Button variant="secondary">PROJECTS</Button></Link>
+                </span>
+                <p>I am a second year Masters student in Computer Science and am looking for an internship to develop my knowledge and experience in the field of machine learning, image processing or both and making use of the skills I have developed through projects in my spare time, work experience and at university.</p>
             </div>
         </div>
     );
