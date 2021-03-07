8c75488d70e59f20859630d76387bc7dd961b5a7
Sebastian Vogt <sebastian1210@web.de>
2021-03-07 16:30:31 +0100

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,12 +11,12 @@ function Home() {
             <HomeCard />
             <div className="aboutme">
                 <h1>Hello</h1>
-                <h2>CS - It's not work, when it makes fun</h2>
+                <h2>CS - It's not work if it's fun</h2>
                 <span className="links">
                     <a href="/cv"><Button>CURRICULUM VITAE</Button></a>
                     <a href="/projects"><Button variant="secondary">PROJECTS</Button></a>
                 </span>
-                <p>I am a second year Masters student in Computer Science and am looking for an internship to develop my knowledge and experience in the field of machine learning, image processing or both and making use of the skills I have developed through projects in my spare time, work experience and at university. (From the length of the sentence you can probably already guess that I am from Germany.)</p>
+                <p>I am a second year Master's student in Computer Science looking for an internship to expand my knowledge and experience in machine learning, image processing, or both, using the skills I have developed through projects in my spare time, work experience, and at university. (From the length of the sentence you can probably already guess that I am from Germany.)</p>
             </div>
         </div>
     );
