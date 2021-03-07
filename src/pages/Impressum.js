acb43e1d11b1eb865b369a81ba9e425c988a6258
Sebastian Vogt <sebastian1210@web.de>
2021-03-07 16:49:29 +0100

    .

--- src/pages/Impressum.js
+++ src/pages/Impressum.js
@@ -5,12 +5,22 @@ const Impressum = () => {
         <div className="Impressum">
             <div className="content">
                 <h1>Impressum</h1>
-                <h2>Zeugs</h2>
-                <p>Small explanatory Text that describes my motivation</p>
-                <h2>Zeugs</h2>
-                <p>Small explanatory Text that describes my motivation</p>
-                <h2>Zeugs</h2>
-                <p>Small explanatory Text that describes my motivation</p>
+                <p>Information according to § 5 TMG</p>
+
+                <p>
+                    Sebastian Vogt<br/>
+                    Fasanenweg 15<br/>
+                    68782 Brühl <br/>
+                    Germany
+                </p>
+
+                <h3>Contact</h3>
+                <p>
+                    Email: <a href="mailto:sebastian1210@web.de">sebastian1210@web.de</a><br/>
+                </p>
+                <h3>Disclaimer</h3>
+                <h4>Liability for links</h4>
+                <p>My offer contains links to external websites of third parties, on whose contents we have no influence. Therefore, we cannot assume any liability for these external contents. The respective provider or operator of the pages is always responsible for the content of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not recognizable at the time of linking. However, a permanent control of the contents of the linked pages is not reasonable without concrete evidence of a violation of the law. If we become aware of any infringements, we will remove such links immediately.</p>
             </div>
         </div>
     )
