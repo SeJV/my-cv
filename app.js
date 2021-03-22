a18fe8caa951831a3050a3f0a3ac544652fc56c6
Sebastian Vogt <sebastian1210@web.de>
2021-03-22 16:07:55 +0100

    .

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 
-const financeRoute = require('./backend_finance/api')
+// const financeRoute = require('./backend_finance/api')
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/api/finance', cors(), financeRoute)
+// app.get('/api/finance', cors(), financeRoute)
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
