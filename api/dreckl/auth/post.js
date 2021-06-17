38acca09702b4f56a24ada33137534f79cdcd077
Sebastian Vogt <sebastian1210@web.de>
2021-06-17 13:45:53 +0200

    .

--- api/dreckl/auth/post.js
+++ api/dreckl/auth/post.js
@@ -5,14 +5,13 @@ const postAuth = (req, res) => {
     const pin = req.body && req.body.pin
     let token = null
     const validPin = 'e7ad40523cbc7d3c04d2873e7af0d6aac50631b203b1a8c792a61511d0fee738'  // sha256(7412).hex
-    console.log(pin)
     if (pin === validPin) {
         // 2. generate jwt
         token = generateAccessToken(req.tokenSecret)
         res.cookie('drecklJWT', token, {maxAge: 1000 * 60 * 60 * 24 * 7})
         res.send('Cookie successfully set')
     } else {
-        res.status(401).send('Pin was wrong')
+        res.status(401).send('Pin was wrong: ' + pin)
     }
 }
 
